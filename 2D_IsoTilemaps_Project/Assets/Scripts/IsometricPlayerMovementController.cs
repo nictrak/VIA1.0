8c40c3fb50f856f2782f92058e5cbeab2ee7901c
nictrak <narin14175@gmail.com>
2021-03-16 21:29:51 +0700

    add spawner

--- 2D_IsoTilemaps_Project/Assets/Scripts/IsometricPlayerMovementController.cs
+++ 2D_IsoTilemaps_Project/Assets/Scripts/IsometricPlayerMovementController.cs
@@ -35,7 +35,6 @@ public class IsometricPlayerMovementController : MonoBehaviour
                 // Destroy(collision.gameObject);
             }
     }
-
     // Update is called once per frame
     void FixedUpdate()
     {
