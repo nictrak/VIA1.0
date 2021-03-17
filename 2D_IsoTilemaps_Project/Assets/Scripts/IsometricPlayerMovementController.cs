5278398ac6664bc4f302d7e020aecc806f483dbd
Kasidit Ratanawongpitak <kasidit_128@hotmail.com>
2021-03-17 20:41:12 +0700

    move combo from Fixupdate to Update

--- 2D_IsoTilemaps_Project/Assets/Scripts/IsometricPlayerMovementController.cs
+++ 2D_IsoTilemaps_Project/Assets/Scripts/IsometricPlayerMovementController.cs
@@ -41,29 +41,17 @@ public class IsometricPlayerMovementController : MonoBehaviour
                 // Destroy(collision.gameObject);
             }
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        // var list = GetComponents(typeof(Component));
-        // for(int i = 0; i < list.Length; i++)
-        // {
-        //     Debug.Log(list[i].ToString());
-        // }
-
-
-
-        Vector2 currentPos = rbody.position;
+	
+	void Update()
+	{
+		Vector2 currentPos = rbody.position;
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
         inputVector = Vector2.ClampMagnitude(inputVector, 1);
         Vector2 movement = inputVector * movementSpeed;
-        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-        isoRenderer.SetDirection(movement);
-        rbody.MovePosition(newPos);	
 		
-		if (Input.GetKey(KeyCode.Z) && curTime + attackDelay < Time.time ){
+		if (Input.GetKeyDown(KeyCode.Z) && curTime + attackDelay < Time.time ){
             // CMDebug.TextPopupMouse("Attack !!!");
 			if (!comboing && !comboing2)
 			{
@@ -93,13 +81,36 @@ public class IsometricPlayerMovementController : MonoBehaviour
                 enermy.GetComponent<MonsterHealth>().TakeDamage(attackDamage);
             }
         }
-		
+				
 		if (( comboing || comboing2 ) && curTime + comboTimer < Time.time)
 		{
 			Debug.Log("Reset !!!");
 			comboing = false;
 			comboing2 = false;
 		}
+		
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // var list = GetComponents(typeof(Component));
+        // for(int i = 0; i < list.Length; i++)
+        // {
+        //     Debug.Log(list[i].ToString());
+        // }
+
+
+
+        Vector2 currentPos = rbody.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        Vector2 movement = inputVector * movementSpeed;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        isoRenderer.SetDirection(movement);
+        rbody.MovePosition(newPos);	
 		
     }
 
