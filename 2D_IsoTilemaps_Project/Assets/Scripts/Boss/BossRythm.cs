ebebcba9bb2a3a66fc4735cbb635e35219d00287
nictrak <narin14175@gmail.com>
2021-04-07 00:13:00 +0700

    Minor fix

--- 2D_IsoTilemaps_Project/Assets/Scripts/Boss/BossRythm.cs
+++ 2D_IsoTilemaps_Project/Assets/Scripts/Boss/BossRythm.cs
@@ -8,17 +8,11 @@ public class BossRythm : MonoBehaviour
     private float triggerHealth;
 
     private MonsterHealth monsterHealth;
-    private Boss boss;
-    private GameObject player;
-    private RythmSystem rythmSystem;
     private bool isAlreadyTrigger;
     // Start is called before the first frame update
     void Start()
     {
         monsterHealth = GetComponent<MonsterHealth>();
-        boss = GetComponent<Boss>();
-        player = GameObject.FindGameObjectWithTag("Player");
-        rythmSystem = GameObject.FindGameObjectWithTag("Rythm").GetComponent<RythmSystem>();
         isAlreadyTrigger = false;
     }
 
@@ -27,13 +21,8 @@ public class BossRythm : MonoBehaviour
     {
         if (!isAlreadyTrigger && monsterHealth.CurrentHealth <= triggerHealth)
         {
-            DisablePlayer();
-            player.transform.position = rythmSystem.CenterPoint.transform.position;
             isAlreadyTrigger = true;
         }
     }
-    private void DisablePlayer()
-    {
-        player.GetComponent<IsometricPlayerMovementController>().enabled = false;
-    }
+
 }
